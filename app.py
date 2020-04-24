fe77e44a72e3fb5840a5c936e6391ceb0065f609
Sheila-Ire <irechukwusc@gmail.com>
2020-04-24 17:39:16 +0100

    python

---
+++ app.py
@@ -0,0 +1,53 @@
+employee_details = {
+    'fname': input('What is your first_name? '),
+    'lname': input('What is your last_name? '),
+    'email': input('What is your email? '),
+}
+print(employee_details)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
